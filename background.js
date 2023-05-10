7140e28fef17167ff39672da8fe6ac8682da6260
rijusougata13 <rijusougata13@gmail.com>
2023-05-10 11:22:57 +0530

    updated popup.html

--- background.js
+++ background.js
@@ -15,6 +15,24 @@
 // };
 // const firebase=initializeApp(firebaseConfig);
 
+let user_signed_in = false;
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.message === 'is_user_signed_in') {
+      sendResponse({
+          message: 'success',
+          payload: user_signed_in
+      });
+  } else if (request.message === 'sign_out') {
+      user_signed_in = false;
+      sendResponse({ message: 'success' });
+  } else if (request.message === 'sign_in') {
+      user_signed_in = true;
+      sendResponse({ message: 'success' });
+  }
+
+  return true;
+});
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.message === "download_status") {
